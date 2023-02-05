d25c4a3952878f5c526f145e0e2ed9673eaabc3b
Argha Das <arghadas@Arghas-MacBook-Air.local>
2023-02-05 21:55:09 +0530

    top sub header

--- pages/_app.js
+++ pages/_app.js
@@ -3,16 +3,27 @@ import "../styles/globals.scss";
 import store from "../store";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
+import Head from "next/head";
 
 let persistor = persistStore(store);
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <Component {...pageProps} />
-      </PersistGate>
-    </Provider>
+    <>
+      <Head>
+        <title>Shoppers Halt</title>
+        <meta
+          name="description"
+          content="Shoppers halt online shopping service for all of your needs."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Component {...pageProps} />
+        </PersistGate>
+      </Provider>
+    </>
   );
 }
 
