8e9375cea4f9ebc2a8efaf0d97b217788e23b49b
Argha Das <arghadas@Arghas-MacBook-Air.local>
2023-02-07 19:30:35 +0530

    dropdown menu for profile over

--- components/header/Top.js
+++ components/header/Top.js
@@ -3,14 +3,19 @@ import { MdSecurity } from "react-icons/md";
 import { BsSuitHeart } from "react-icons/bs";
 import { RiAccountPinCircleLine, RiArrowDownSFill } from "react-icons/ri";
 import Link from "next/link";
+import { useState } from "react";
+import UserMenu from "./UserMenu";
 
 export default function Top() {
+  const [loggedIn, setLoggedIn] = useState(true);
+  const [visible, setVisible] = useState(false);
+
   return (
     <div className={styles.top}>
       <div className={styles.top__container}>
         <div></div>
         <ul className={styles.top__list}>
-          <li>
+          <li className={styles.top__listItem}>
             <img
               src="https://upload.wikimedia.org/wikipedia/en/thumb/4/41/Flag_of_India.svg/1200px-Flag_of_India.svg.png"
               alt="flag"
@@ -18,29 +23,45 @@ export default function Top() {
             <span>India / INR</span>
           </li>
 
-          <li>
+          <li className={styles.top__listItem}>
             <MdSecurity />
             <span>Buyer Protection</span>
           </li>
 
-          <li>
+          <li className={styles.top__listItem}>
             <span>Customer Service</span>
           </li>
-          <li>
+          <li className={styles.top__listItem}>
             <span>Help</span>
           </li>
-          <li>
+          <li className={styles.top__listItem}>
             <BsSuitHeart />
             <Link href="/profile/wishlist">
               <span>Wishlist</span>
             </Link>
           </li>
-          <li>
-            <div className={styles.flex}>
-              <RiAccountPinCircleLine />
-              <span>Account</span>
-              <RiArrowDownSFill />
-            </div>
+          <li
+            className={styles.top__listItem}
+            onMouseOver={() => setVisible(true)}
+            onMouseLeave={() => setVisible(false)}
+          >
+            {loggedIn ? (
+              <div className={styles.flex}>
+                <img
+                  src="https://d2qp0siotla746.cloudfront.net/img/use-cases/profile-picture/template_0.jpg"
+                  alt=""
+                />
+                <span>Argha</span>
+                <RiArrowDownSFill />
+              </div>
+            ) : (
+              <div className={styles.flex}>
+                <RiAccountPinCircleLine />
+                <span>Account</span>
+                <RiArrowDownSFill />
+              </div>
+            )}
+            {visible && <UserMenu loggedIn={loggedIn} />}
           </li>
         </ul>
       </div>
---
+++ components/header/UserMenu.js
@@ -0,0 +1,47 @@
+import Link from "next/link";
+import styles from "./styles.module.scss";
+
+export default function UserMenu({ loggedIn }) {
+  return (
+    <div className={styles.menu}>
+      <h4>Welcome to Shoppers Halt</h4>
+      {loggedIn ? (
+        <div className={styles.flex}>
+          <img
+            src="https://d2qp0siotla746.cloudfront.net/img/use-cases/profile-picture/template_0.jpg"
+            alt=""
+            className={styles.menu__img}
+          />
+          <div className={styles.col}>
+            <span>Welcome Back,</span>
+            <h3>Argha</h3>
+            <span>Sign Out</span>
+          </div>
+        </div>
+      ) : (
+        <div className={styles.flex}>
+          <button className={styles.btn_primary}>Register</button>
+          <button className={styles.btn_outlined}>Login</button>
+        </div>
+      )}
+
+      <ul>
+        <li>
+          <Link href="/profile">Account</Link>
+        </li>
+        <li>
+          <Link href="/profile/orders">My Orders</Link>
+        </li>
+        <li>
+          <Link href="/profile/messages">Message Center</Link>
+        </li>
+        <li>
+          <Link href="/profile/address">Address</Link>
+        </li>
+        <li>
+          <Link href="/profile/wishlist">Wishlist</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
